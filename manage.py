b6bdb89c39ab3b20421be66e1da177420f5d7484
Aditya <aditya.sharma263@gmail.com>
2019-01-27 18:34:56 +0530

    seed removed

--- manage.py
+++ manage.py
@@ -4,14 +4,14 @@ from flask_script import Manager, Server
 from flask_migrate import Migrate, MigrateCommand
 from stay_app import db, app
 from stay_app.cron import cron
-from stay_app.seed import SeedData
+# from stay_app.seed import SeedData
 
 migrate = Migrate(app, db)
 
 manager = Manager(app)
 manager.add_command('db', MigrateCommand)
 manager.add_command('cron', cron)
-manager.add_command('seed', SeedData)
+# manager.add_command('seed', SeedData)
 manager.add_command("runserver", Server(use_debugger=app.config['SERVER_DEBUG'],
                                         use_reloader=app.config['SERVER_RELOAD'],
                                         host=app.config['SERVER_HOST'],
