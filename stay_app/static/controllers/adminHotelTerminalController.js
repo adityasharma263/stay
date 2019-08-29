95b7b2a37e54963de4d38272c49d53f1adb5f1ee
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2019-08-29 12:15:22 +0530

    update: ng-app=stay

--- stay_app/static/controllers/adminHotelTerminalController.js
+++ stay_app/static/controllers/adminHotelTerminalController.js
@@ -1,62 +1,94 @@
 // add dependency module DYNAMICALLY
 app.requires.push('ui.bootstrap');
-app.controller("adminHotelTerminalController", function($scope, $http) {
+app.controller("adminHotelTerminalController", function ($scope, $http) {
+
+    $scope.roomPriceStructureB2B = {};
 
 
-    $http.get("/api/v1/hotel")
-    .then(function(response) {
-        
-        $scope.hotelDetails = response.data.result.hotel;
-        console.log($scope.hotelDetails);
-    })
-    .catch(function(err) {
-        console.log(err);
-    });
+    $http.get("/api/v1/hotel/b2b")
+        .then(function (response) {
+
+            $scope.hotelDetails = response.data.result.hotel;
+            console.log($scope.hotelDetails);
+
+        })
+        .catch(function (err) {
+            console.log(err);
+        });
 
 
-    $scope.searchHotelOnChange = function(){
-        if($scope.searchHotel){
+        $scope.getRoomPriceByID  = function(roomID){
+
+            // $http.get("/api/v1/deal",
+            // { params: { room_id: roomID, order_by : loadRoomPriceFor} })
+            // .then(function(response) {
+            //     console.log("loadRoomPriceFor = ",response.data);
+            // }).catch(function(err) {
+            //     console.log(err);
+            // });
+
+
+            console.log("roomID = ",roomID);
+
+
+
+        };
+    
+
+
+    $scope.searchHotelOnChange = function () {
+        if ($scope.searchHotel) {
             console.log($scope.searchHotel);
-            $http.post("/admin/hotel/search", {search : $scope.searchHotel})
-            .then(function(respsonse){
-                console.log(respsonse);
-                $scope.searchResult = respsonse.data.result.names;
-            })
-            .catch(function(err) {
-                console.log("err ", err);
-            });
+            $http.post("/admin/hotel/search", { search: $scope.searchHotel })
+                .then(function (respsonse) {
+                    console.log(respsonse);
+                    $scope.searchResult = respsonse.data.result.names;
+                })
+                .catch(function (err) {
+                    console.log("err ", err);
+                });
         }
     };
-    
-    // $scope.hotelDetails = hotelDetails.hotel[0];
 
-    
+
     $scope.onHotelSelect = function (item) {
         console.log(item);
-        $http.get(API_BASE_URL+"/api/v1/hotel", {params : {id : item.id}})
-        .then(function(response) {
-            console.log(response.data.result);
-            $scope.hotelDetails = response.data.result.hotel[0];
-        })
-        .catch(function(err) {
-            console.log(err);
-        })
-        ;
+        $http.get(API_BASE_URL + "/api/v1/hotel", { params: { id: item.id } })
+            .then(function (response) {
+                console.log(response.data.result);
+                $scope.hotelDetails = response.data.result.hotel[0];
+            })
+            .catch(function (err) {
+                console.log(err);
+            })
+            ;
     };
 
-    $scope.loadRoomPartners = function(hotelId) {
-        console.log("hotelId = ", hotelId);
+    $scope.loadRoomPrice = function (roomID, loadRoomPriceFor) {
+
+        console.log("hotelId = ", roomID);
+
+        $http.get("/api/v1/deal",
+            { params: { room_id: roomID, order_by : loadRoomPriceFor} })
+            .then(function(response) {
+                console.log("loadRoomPriceFor = ",response.data);
+            }).catch(function(err) {
+                console.log(err);
+            });
+
 
 
     };
 
-    $scope.changeSitePrice = function(e) {
+    $scope.changeSitePrice = function (e) {
         console.log(e.keyCode);
 
-        if(e.keyCode == 13){
+        if (e.keyCode == 13) {
 
             $("#confirmModal").modal('show');
 
         }
     };
+
+
 });
