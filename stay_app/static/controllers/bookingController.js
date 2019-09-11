933084680d745806fcadcc11894f890cc1b7464e
Akshay Rana Gujjar <akshaybhati28@gmail.com>
2019-09-11 15:06:48 +0530

    add: booking

--- stay_app/static/controllers/bookingController.js
+++ stay_app/static/controllers/bookingController.js
@@ -10,11 +10,12 @@ app.controller('bookingController', ["$scope", "$http", function ($scope, $http)
         deal: []
     }];
 
+    //  what is the use of this variable
     $scope.customerDetailsArrayIndex;
 
     // here we are creating a array of arraies of objects, 
     // yes bit complicated 
-    // but it is usefull when user remove and customer from the customer array, 
+    // but it is usefull when user remove customer from the customer array, 
     // especially when user remove from in between from the array
     $scope.customersSelectedRooms = [[]];
 
@@ -48,6 +49,33 @@ app.controller('bookingController', ["$scope", "$http", function ($scope, $http)
                         "sold_out": false,
                     },
 
+                },
+                {
+                    "cart": 1,
+                    "ci_date": "2019-08-30T10:50:49.783203+00:00",
+                    "co_date": "2019-08-30T10:50:49.783203+00:00",
+                    "current_deal_amount": 300,
+                    "id": 1,
+                    "no_of_deals": 2,
+                    "deal": {
+                        "id": 1,
+                        "partner_id": 1,
+                        "price": 7000,
+                        "room": {
+                            "hotel": {
+                                "city": "delhi",
+                                "id": 1,
+                                "name": "lalee"
+                            },
+                            "id": 1,
+                            "image_url": "https://seoimgak.mmtcdn.com/blog/sites/default/files/images/hotels-in-mahabaleshwar-saj%20resort.jpg",
+                            "max_no_of_guest": null,
+                            "meal_plan": "Continental Plan",
+                            "room_type": null
+                        },
+                        "sold_out": false,
+                    },
+
                 }
             ],
             "id": 1,
@@ -67,8 +95,8 @@ app.controller('bookingController', ["$scope", "$http", function ($scope, $http)
             deal: []
         });
 
-        if ($scope.customersSelectedRooms[customerDetailsArrayIndex] == undefined) {
-            $scope.customersSelectedRooms[customerDetailsArrayIndex] = [];
+        if ($scope.customersSelectedRooms[$scope.customerDetailsArrayIndex] == undefined) {
+            $scope.customersSelectedRooms[$scope.customerDetailsArrayIndex] = [];
 
         } else {
             $scope.customersSelectedRooms.push([]);
@@ -106,87 +134,132 @@ app.controller('bookingController', ["$scope", "$http", function ($scope, $http)
 
 
     $scope.bookingData = {
-        
-		"business_contact_no": null, 
-		"gst_no": null, 
-        "office_address": null, 
-        "business_email": null, 
-        "company_name": null, 
-        "contact_no": null, 
+
+        "business_contact_no": null,
+        "gst_no": null,
+        "office_address": null,
+        "business_email": null,
+        "company_name": null,
+        "contact_no": null,
         "deals": [
-          {
-            "base_booking_price": null, 
-            "base_price": null, 
-            "ci_date": null, 
-            "co_date": null, 
-            "commission_in_percentage": null, 
-            "contact_no": null, 
-            "current_deal_amount": null, 
-            "email": null, 
-            "final_price": null, 
-            "guest_fist_name": null, 
-            "guest_last_name": null, 
-            "margin_price": null, 
-            "partner_id": 2, //jis partner se kri h 
-            "room_id": 1, 
-            "ts_exclusive": null
-          }
-        ], 
+            {
+                "base_booking_price": null,
+                "base_price": null,
+                "ci_date": null,
+                "co_date": null,
+                "commission_in_percentage": null,
+                "contact_no": null,
+                "current_deal_amount": null,
+                "email": null,
+                "final_price": null,
+                "guest_fist_name": null,
+                "guest_last_name": null,
+                "margin_price": null,
+                "partner_id": 2, //jis partner se kri h 
+                "room_id": 1,
+                "ts_exclusive": null
+            }
+        ],
         "partner_id": 1, //jis partner ne kri h jo login h partner
         "total_booking_amount": null
-      };
+    };
 
 
-$scope.finalAmount;
+    $scope.finalAmount;
 
 
-$scope.qunatityChange = function() {
-    $scope.finalAmount = $scope.finalQunatity * $scope.cartDetails[0].cart_deals[0].deal.price;
-}
+    $scope.qunatityChange = function () {
+        $scope.finalAmount = $scope.finalQunatity * $scope.cartDetails[0].cart_deals[0].deal.price;
+    }
 
 
 
 
-$scope.processBook = function() {
-
-    $scope.bookingData.business_email 
-    = $scope.bookingData.deals[0].email =
-    $scope.customerDetails[0].email;
-
-    $scope.bookingData.deals[0].guest_fist_name =
-    $scope.customerDetails[0].guest_fist_name;
-
-    $scope.bookingData.deals[0].guest_last_name =
-    $scope.customerDetails[0].guest_last_name;
-
-    $scope.bookingData.contact_no 
-    = $scope.bookingData.deals[0].contact_no
-    = $scope.customerDetails[0].contact_no;
-
-    $scope.bookingData.company_name = $scope.customerDetails[0].guest_fist_name +" "+ $scope.customerDetails[0].guest_last_name;
-    
-    
-    $scope.bookingData.deals[0].room_id;
-
-    $scope.bookingData.total_booking_amount = $scope.finalAmount;
+    $scope.processBook = function () {
 
 
-    console.log($scope.bookingData);
+        console.log("$scope.customer = ", $scope.customerDetails);
+        console.log("$scope.customersSelectedRooms = ", $scope.customersSelectedRooms);
 
-    // return;
-    $http.post("/hotel/booking", $scope.bookingData)
-    .then(function(response) {
-        console.log(response.data);
+        for (let i = 0; i < $scope.customerDetails.length; i++) {
+            const customer = $scope.customerDetails[i];
 
-        $("#repsonse").html(response.data);
+            console.log(`customer ${i} = `, customer);
+
+            if ($scope.customersSelectedRooms[i])
+                for (let j = 0; j < $scope.customersSelectedRooms[i].length; j++) {
+                    const bookingDeal = $scope.customersSelectedRooms[i][j];
 
 
-    })
-    .catch(function(err) {
-        console.log("err = ", err);
-    })
-}
+                    console.log(`bookingDeal ${j} = `, bookingDeal);
 
+
+                }
+        }
+
+
+
+        return;
+
+        $scope.bookingData.business_email
+            = $scope.bookingData.deals[0].email =
+            $scope.customerDetails[0].email;
+
+        $scope.bookingData.deals[0].guest_fist_name =
+            $scope.customerDetails[0].guest_fist_name;
+
+        $scope.bookingData.deals[0].guest_last_name =
+            $scope.customerDetails[0].guest_last_name;
+
+        $scope.bookingData.contact_no
+            = $scope.bookingData.deals[0].contact_no
+            = $scope.customerDetails[0].contact_no;
+
+        $scope.bookingData.company_name = $scope.customerDetails[0].guest_fist_name + " " + $scope.customerDetails[0].guest_last_name;
+
+
+        $scope.bookingData.deals[0].room_id;
+
+        $scope.bookingData.total_booking_amount = $scope.finalAmount;
+
+
+        console.log($scope.bookingData);
+
+        // return;
+        $http.post("/hotel/booking", $scope.bookingData)
+            .then(function (response) {
+                console.log(response.data);
+
+                $("#repsonse").html(response.data);
+
+
+            })
+            .catch(function (err) {
+                console.log("err = ", err);
+            });
+    };
+
+
+    $scope.log = function (logStatement) {
+
+        console.log("logStatement = ", logStatement);
+
+    };
+
+    //  not using this fn delete it
+    $scope.getTotalAmoutForCustomer = function (dealWithQuantity) {
+
+
+        dealWithQuantity.reduce((total, nextValue) => {
+
+        });
+
+    };
+
+    // TO DO:
+    // Get Customer details from $scope.customer
+    // get customer rooms details from $scope.customersSelectedRooms
+    // get the value of customer and rooms from same index 
 
 
 }]);
