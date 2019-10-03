113339f268e0d71fd6fac12f0c5567d05ba0c1f4
aditya <aditya.sharma263@gmail.com>
2019-10-03 17:15:44 +0530

    cart get api change

--- stay_app/view/hotel.py
+++ stay_app/view/hotel.py
@@ -724,9 +724,9 @@ def cart_api():
                 if deal:
                     total_deals = total_deals + deal.no_of_deals
                     deal_data = requests.get(url=str(app.config["API_URL"]) + "/api/v1/deal", params={"id": deal.deal_id,
-                                                                                                      "ci": deal.ci_date,
-                                                                                                      "co": deal.co_date})
-                    deal.current_deal_amount = int(deal_data.json()["result"]["deal"][0]["price"]) * deal.no_of_deals
+                                                                                                      "start_date":  int(time.mktime(time.strptime(str(deal.ci_date)[:19], "%Y-%m-%d %H:%M:%S"))),
+                                                                                                      "end_date":  int(time.mktime(time.strptime(str(deal.co_date)[:19], "%Y-%m-%d %H:%M:%S")))})
+                    deal.current_deal_amount = int(deal_data.json()["result"]["deal"][0].get('price', 0)) * deal.no_of_deals
                     total_amount = total_amount + deal.current_deal_amount
 
                     hotel = q.filter(Deal.id == deal.deal_id).first()
